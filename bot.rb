a3a2cd2cea43067bfed901b981f68b1006dcc040
root <root@hantower.localdomain>
2022-02-08 23:24:25 -0800

    initial commit

---
+++ bot.rb
@@ -0,0 +1,23 @@
+require 'telegram_bot'
+
+token = ''
+
+bot = TelegramBot.new(token: token)
+
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username|: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /start/i
+      reply.text = "all i do is say what'sup bitch. try /greet hoe"
+    when /greet/i
+      reply.text = "Hello, #{message.from.first_name}.
+    else
+      reply.text = "ionno no #{command.inspect} mufuka. try that shit again."
+    end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+  end
+end
